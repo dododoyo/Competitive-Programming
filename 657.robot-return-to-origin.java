74950326ed1392e6b64b50992fdd8ce1792197e4
Doyoo <dolphinmulugeta88@gmail.com>
2023-01-03 06:48:51 +0000

    My first commit

--- 657.robot-return-to-origin.java
+++ 657.robot-return-to-origin.java
@@ -1,4 +1,30 @@
-public class Solution
-{
+class Solution {
+    public boolean judgeCircle(String moves) 
+    {
 
+        int upCounter = 0;
+        int downCounter = 0;
+        int leftCounter = 0;
+        int rightCounter = 0;
+        for(int i = 0; i < moves.length() ; i++)
+        {
+            if (moves.charAt(i) == 'L')
+            leftCounter++;
+            else if (moves.charAt(i) == 'U')
+            upCounter++;
+            else if (moves.charAt(i) == 'D')
+            downCounter++;
+            else if (moves.charAt(i) == 'R')
+            rightCounter++;
+
+        }
+
+        if (downCounter == upCounter && leftCounter == rightCounter)
+        return true;
+        else;
+        return false;
+        
+    }
 }
+// @lc code=end
+
