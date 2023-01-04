e6a178f209bd6ceb23960cdadd5b873586497cd1
Doyoo <dolphinmulugeta88@gmail.com>
2023-01-04 20:55:04 +0000

    runtime 70.72 %(2 ms) memory usage 45.9 %(54.1 MB)

---
+++ 941.valid-mountain-array.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+
+        boolean flag1 = true,flag2 = true;
+
+        int maxIndex = 0,max = 0;
+
+        if (arr.length < 3)
+        return false;
+
+    
+
+
+        for (int i = 0 ; i < arr.length ;i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        if(maxIndex == arr.length-1 || maxIndex == 0)
+        return false;
+
+        int j =0;
+        while (j < maxIndex -1 )
+        {
+            if (arr[j] >= arr[j+1])
+            flag1 = false;
+            j++;
+        }
+
+        j = maxIndex;
+
+        if (!flag1)
+        return false;
+
+        while (j < arr.length -1 )
+        {
+            if (arr[j] <= arr[j+1])
+            flag1 = false;
+            j++;
+        }
+
+        if (flag1 && flag2)
+        return true;
+        else return false;
+
+    }
+}
+
