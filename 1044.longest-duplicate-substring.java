a2055c2f5a0cd696f5c26b5c70515c48e200c5ae
Doyoo <dolphinmulugeta88@gmail.com>
2023-01-04 19:41:02 +0000

    hjgjhgkj

---
+++ 1044.longest-duplicate-substring.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1044 lang=java
+ *
+ * [1044] Longest Duplicate Substring
+ */
+
+// @lc code=start
+class Solution 
+{
+    public String longestDupSubstring(String s) {
+        
+    }
+}
+// @lc code=end
+
