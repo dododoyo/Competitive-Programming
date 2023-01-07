2dfdff148414d918379a0503b0fb8718d77e7bbd
Doyoo <dolphinmulugeta88@gmail.com>
2023-01-07 20:46:45 +0000

    Runtime(97.31%) 9ms : Memory usage(92.76%) 41.5MB

---
+++ 9.palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        int newNumber =0 ,result,temp = x;
+        if (x < 0)
+        x *= -1;
+        
+        while(x!=0)
+        {
+            result = x%10;
+            newNumber = newNumber*10 + result;
+            x = x /10;
+        }  
+
+        if (temp == newNumber)
+            return true;
+        else
+            return false;
+    }
+}
+
