702d7fb9c06ae4464130d8adf17ab5d58f9a49bd
Doyoo <dolphinmulugeta88@gmail.com>
2023-01-13 22:04:46 +0000

    Solved Domino Piling

---
+++ DominoPiling.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class DominoPiling
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> thenation = new ArrayList<>();
+        thenation.
+        Scanner kb = new Scanner(System.in);
+        int first = kb.nextInt();
+        int second = kb.nextInt();
+        
+        System.out.println( (first*second)/2);
+
+    }
+}
