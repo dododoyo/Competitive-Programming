61c5c760f41697290633be0e16caa0559aad477f
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-02-10 16:10:16 +0000

    Time: 145 ms (94.85%), Space: 15.2 MB (40.55%)

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums):
+        S = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (S - leftsum - x):
+                return i
+            leftsum += x
+        return -1
