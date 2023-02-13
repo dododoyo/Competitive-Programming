37f745ed9468937e6d08dc8357cbc8987a641fec
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-02-13 23:31:05 +0000

    Time: 27 ms (94.24%), Space: 14 MB (8.72%)

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        #let's use two pointer each pointing at the start of the 
+        #two strings
+        
+        if(len(s) == 0):
+            return True
+        
+        p1 = 0
+        p2 = 0
+        
+        while(p2 < len(t) and p1 < len(s)):
+            if(t[p2] == s[p1]):
+                p1 += 1
+                
+            
+            p2 += 1
+            
+        
+        return p1 == len(s)
