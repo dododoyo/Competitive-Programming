8a89ea7e074d26718b0d51182d134b1fd266e6d8
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-03 15:13:10 +0000

    Time: 57 ms (38.39%), Space: 14.7 MB (67.97%)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,31 +1,22 @@
-#
-# @lc app=leetcode id=125 lang=python3
-#
-# [125] Valid Palindrome
-#
-
-# @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #lets use two pointer pointed at and and begining
-        # to check if characters at pointer are same
-        startPointer = 0
+        p1 = 0
+        p2 = len(s) - 1
+        s = s.lower()
         
-        s = re.sub("[^a-z0-9]", "", s.lower())
-        
-        endPointer = len(s) - 1
-        print(s)
-        while (startPointer < endPointer):  
-            if(s[startPointer] == s[endPointer]):
-                startPointer += 1
-                endPointer -= 1
-            else:
+        while(p1 < p2):
+            while((p1 < p2) and (not s[p1].isalnum())):
+                p1 += 1
+                continue
+            
+            while((p1 < p2) and (not s[p2].isalnum())):
+                p2 -= 1
+                continue
+                
+            if(s[p1] != s[p2]):
                 return False
-           
+            
+            p1 += 1
+            p2 -= 1
         return True
-
-
-
         
-# @lc code=end
-
