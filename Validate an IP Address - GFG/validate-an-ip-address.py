8264a0aaaa6c3e677d02fe4b37b97c779987ec03
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-09 17:14:36 +0300

    Added solution

---
+++ Validate an IP Address - GFG/validate-an-ip-address.py
@@ -0,0 +1,57 @@
+#User function Template for python3
+def isValid(s):
+    #code here
+    
+    #checking if input starts  and ends with "."
+    
+    if s[0] == '.' or s[-1] == ".":
+        return 0
+        
+    each_IP = s.split(".")
+    
+    len_ = len(each_IP)
+    
+    #testing if string doesn't contain 4 numbers
+    if(len_ != 4):
+        return 0
+    
+    for i in range(len_):
+        
+        each_str = each_IP[i]
+        
+        #testing for empty strings
+        if each_str == "":
+            return 0
+        #testing if strings are not numbers
+        if not each_str.isnumeric():
+            return 0
+            
+        #testing for leading zeros
+        if (len(each_str) > 1) and (each_str[0] == "0"):
+            return 0
+            
+        each_num = int(each_str)
+        
+        #testing if each nums is within range
+        if (not (each_num > -1 and each_num < 256 )): 
+            return 0
+            
+    return 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        if(isValid(s)):
+            print(1)
+        else:
+            print(0)
+    
+
+# } Driver Code Ends
