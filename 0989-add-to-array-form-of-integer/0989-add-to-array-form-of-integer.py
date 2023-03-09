6c172709ce7fd845e09eaa22034f6e59ecec663c
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-09 23:41:39 +0300

    Time: 251 ms (99.54%), Space: 15.1 MB (32.78%)

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -6,21 +6,34 @@ class Solution:
         index = 0
         
         while k:
+            #get the least index
             least_k = k%10
-                
+               
+            # if index is within the number add it in position
             if index < num_len:
                 num[index] += least_k
+                
+            # if solution may need more space than num?
+            # append it to the end
             else:
                 num.append(least_k)
             
+            # get the carry element
             carry = num[index] // 10
+            
+            #update the index element with it's correct value
             num[index] = num[index] % 10
             
+            #update k
             k = k//10
+            
+            #add carry element to k
             k += carry
             
+            #increase the index of array
             index += 1
-            
+        
+        #reverse the num
         num.reverse()
 
         return num
