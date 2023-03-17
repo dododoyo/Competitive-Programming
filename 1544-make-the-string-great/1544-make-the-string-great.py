e29a53c48e3506f8cf7352e414b4f42ded9b82b3
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-17 16:58:52 +0300

    Time: 39 ms (49.75%), Space: 13.8 MB (49.67%)

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,22 +1,24 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        s_len = len(s)
-        
-        #take care of some base cases
-        if(s_len == 1):
-            return s
-        
         stk = []
         
         for i in s:
-            if(len(stk) == 0):
+            if(not stk):
                 stk.append(i)
                 
             else:
-                #easier way to check if not great will be using ascii value
+                # if the last element checked and the element after it are
+                # not good remove the one in the stack and don't append 
+                # the one that is checked now
                 if(abs(ord(stk[-1]) - ord(i)) == 32):
+                # easier way to check if not great will be using ascii value
                     stk.pop()
+                
+                # if they are good it can be added to the string
                 else:
                     stk.append(i)
 
         return "".join(stk)
+    
+        #time O(n)
+        #space O(n)
