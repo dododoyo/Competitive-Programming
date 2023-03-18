26ab2b3ed63d5590025bd6bc54e253c41902fbf7
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-18 20:43:02 +0300

    Time: 174 ms (5.83%), Space: 14.8 MB (87.95%)

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -3,16 +3,18 @@ class Solution:
         #let's use two pointers
         p2 = len(s) - 1
         p1 = 0
-        vowels = ['a','e','i','o','u','A','E','I','O','U']
+        vowels = "aeiouAEIOU"
         sol = ""
         
         if(p2 == 0):
             return s
         
+        #using string of vowels to check it it run faster
         while(p1 < p2):
             if (s[p1] not in vowels and s[p2] not in vowels):
                 p1 += 1
                 p2 -= 1
+                
             elif (s[p1] in vowels and s[p2] not in vowels): p2 -= 1
             elif (s[p1] not in vowels and s[p2] in vowels): p1 += 1  
             else:
