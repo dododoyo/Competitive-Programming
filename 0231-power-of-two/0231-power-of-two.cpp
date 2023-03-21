b1253ff69a40f1cd2da435838a9dcecba1f7a580
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-21 20:52:08 +0300

    Time: 3 ms (52.82%), Space: 5.8 MB (83.64%)

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if(n < 0)
-            return false;
-    
-        while(n>1)
-        {
-            if(n%2 == 0)
-            {
-                n = n/2;
-            }
-            else
-            {
-                return false;
-            }
-        }
         
-        if(n== 0)
-            return false;
-    
-        return true;
+        // let's use recurssion
         
+        //if the number went down two  1 then it is divisible by two
+        if(n == 1){return true;}
+        
+        // if remainder of a number divided by two 
+        // is not zero then it is not divisible by two
+        if(n == 0 or n%2 != 0){return false;}
+        
+        //if the number is not one and 
+        //not zero and it remainder is zero means its divisible
+        //by two check the number all the way down to 1 using recurrsion
+        return isPowerOfTwo(n/2);
     }
 };
