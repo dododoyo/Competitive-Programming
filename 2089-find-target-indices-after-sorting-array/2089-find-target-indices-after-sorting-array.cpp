5efe9bfa90eb477c9240f7c85f18c1346a34e392
Dolphin Mulugeta <94123357+Dooyo@users.noreply.github.com>
2023-03-23 17:50:56 +0300

    Time: 0 ms (100.00%), Space: 11.8 MB (21.40%)

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) 
+    {
+        bool isNotPresent = false;
+        vector<int> freq(101);
+        
+        for(int i = 0; i < nums.size();i++){freq[nums[i]] += 1;}
+        for(int i = 1; i < freq.size();i++){freq[i] = freq[i] + freq[i-1];}
+        
+        vector<int> solution;
+        if(freq[target] > 0)
+        {
+            
+                for(int i = freq[target-1] ; i < freq[target];i++)                                                  {solution.insert(solution.end(),i);}
+
+        }
+        else{
+            return solution;
+        }
+        
+        
+        return solution;
+    }
+};
