76d4dd78c55e76d74bf42606951a052bcd7af345
Dolphin Mulugeta <94123357+dododoyo@users.noreply.github.com>
2023-04-12 14:49:35 +0300

    Time: 289 ms (58.30%), Space: 14.3 MB (45.55%)

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -2,16 +2,15 @@ from collections import defaultdict
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         freq = defaultdict(int)
+        #maxes holds most frequent number(maxes[1]) and it's frequency(maxes[0])
+        maxes = [0,-1]
         for i in nums:
             if i%2 == 0:
                 freq[i] += 1
-        maxes = [0,-1]
-        for i in freq:
-            if freq[i] > maxes[0]:
-                maxes[0] = freq[i]
-                maxes[1] = i
-            elif freq[i] == maxes[0]:
-                maxes[0] = freq[i]
-                maxes[1] = min(maxes[1],i)
+                if maxes[0] < freq[i]:
+                    maxes[0] = freq[i]
+                    maxes[1] = i
+                elif maxes[0] == freq[i]:
+                    maxes[1] = min(i,maxes[1])
                 
         return maxes[1]
