44fa6f250099576f6b122e42306030e878cfcc4c
Dolphin Mulugeta <94123357+dododoyo@users.noreply.github.com>
2023-04-25 23:17:30 +0300

    Time: 49 ms (58.46%), Space: 14.1 MB (57.34%)

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,3 +1,12 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1)&set(nums2))
+        nums1.sort();nums2.sort()
+        solution = []
+        i,j = 0,0
+        while(i < len(nums1) and j < len(nums2)):
+            if nums1[i] > nums2[j]:j+=1;
+            elif nums1[i] < nums2[j]:i+=1;
+            elif nums1[i] == nums2[j]:
+                solution.append(nums1[i]);i+=1;j+=1;
+        return list(set(solution));
+                
