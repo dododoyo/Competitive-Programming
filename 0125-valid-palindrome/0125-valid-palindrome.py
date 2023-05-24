e77b8b9e748fc3b5734b5504af8ceec53950d01b
Dolphin Mulugeta <94123357+dododoyo@users.noreply.github.com>
2023-05-24 19:41:40 +0300

    Time: 55 ms (59.51%), Space: 17.1 MB (28.86%)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,22 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        p1 = 0
-        p2 = len(s) - 1
+        '''we can use two pointers pointed at the end
+        and at the begining to check if the strings are 
+        the same and if they are alphanumerical'''
+        #initialize pointers
+        p1,p2= 0,len(s) - 1
+        #convert every element of string to lowercase
         s = s.lower()
         
+        #loop to check if string is valid palindrome
         while(p1 < p2):
-            if (not s[p1].isalnum()):
-                p1 += 1
-                continue
-            
-            if (not s[p2].isalnum()):
-                p2 -= 1
-                continue
-                
-            if(s[p1] != s[p2]):
-                return False
-            
-            p1 += 1
-            p2 -= 1
+            if (not s[p1].isalnum()):p1 += 1;continue
+            if (not s[p2].isalnum()):p2 -= 1;continue
+            if(s[p1] != s[p2]):return False
+            p1 += 1;p2 -= 1
         return True
         
