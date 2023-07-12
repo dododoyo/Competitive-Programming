1086b2ae077ed3b735e8c209535147c083a1bd17
Doyoo <dolphinmulugeta88@gmail.com>
2023-07-12 13:54:08 -0300

    added contest -07 and -08

--- !CodeForces/Contest-06/D_A_2_SV_Verses.py
+++ !CodeForces/Contest-06/D_A_2_SV_Verses.py
@@ -1,12 +1,14 @@
 #failed on test 3
 def count_subarrays(n, a, b, problems):
     l=eligible_count = 0 
-    prefix_sum = [0] * (n + 1)
+    prefix_sum = [0]*(n+1)
     for i in range(1, n + 1):
-        prefix_sum[i] = prefix_sum[i - 1] + problems[i - 1]
-    for r in range(n+1):
-        while prefix_sum[r]-prefix_sum[l]>b:l+=1
-        if a<= prefix_sum[r]-prefix_sum[l] <= b:eligible_count+=1;l=r
+        prefix_sum[i] = prefix_sum[i-1] + problems[i-1]
+    for r in range(n):
+        while prefix_sum[r+1]-prefix_sum[l]>b:l+=1
+        #print(l,r)
+        if (a<= prefix_sum[r+1]-prefix_sum[l] <= b):
+            l=r+1;eligible_count+=1
     return eligible_count
 
 n,a,b = map(int,input().split())
