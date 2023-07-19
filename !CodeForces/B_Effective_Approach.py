8ea4aec4debfefe94df8ea5fef4fe66da930bfb8
dododoyo <dolphinmulugeta88@gmail.com>
2023-07-19 12:01:59 -0400

    solved effective-approach(CF)

--- !CodeForces/B_Effective_Approach.py
+++ !CodeForces/B_Effective_Approach.py
@@ -1,20 +1,25 @@
-def vasya_and_petya_approach(a,e):
-    for i in range(len(a)):
-        if a[i] == e:
-            return i+1 , len(a)-i
-
-
 def effective_approach():
-    #counters for vasya and petya
-    v,p = 0,0
     n = int(input()) #len of list
     a = [int(i) for i in input().split()]
     input()#no need to get the number of queries we can get it from the len of the queries list
+    
+    #arrays to register each elements index in each's perspective
+    v_method = [0]*len(a)
+    p_method = [0]*len(a)
+
+    #initialize counters
+    v,p=0,0
     queries = [int(i) for i in input().split()]
+
+    #register the amount of moves needed for each element
+    for j in range(len(a)):
+        v_method[a[j]-1] = j+1
+        p_method[a[j]-1] = len(a)-j
+
     for i in queries:
-        v += vasya_and_petya_approach(a,i)[0]
-        p += vasya_and_petya_approach(a,i)[1]
-    #return results
+        v+=v_method[i-1]
+        p+=p_method[i-1]
+    #print results
     print(v,p)
 
 effective_approach()
