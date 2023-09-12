14b1985870a73d9d0ff15412b10dcb9c7a2a87f0
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-09-12 14:30:11 -0400

    Time: 51 ms (57.05%), Space: 18.3 MB (90.14%)

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        depth = 1
+        for child in root.children:
+            depth = max(depth, self.maxDepth(child) + 1)
+        return depth
