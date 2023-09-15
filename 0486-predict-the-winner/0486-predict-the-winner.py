87d64153e3411a317df4b4f201397e8cece768b8
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-09-15 12:49:24 -0400

    Time: 2822 ms (19.22%), Space: 16.3 MB (83.71%)

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,13 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        def helper(l,r):
+            if l == r:
+                return nums[l]
+            leftPick = nums[l] - helper(l+1,r);
+            rightPick = nums[r] - helper(l,r-1);-1
+            return max(leftPick,rightPick);
+        if helper(0,len(nums)-1) >=0:
+            return 1
+        return 0
+            
