37636398693a3615be8c98e799ca6a2cbeed389e
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-09-28 23:26:06 +0300

    Time: 68 ms (76.57%), Space: 16.6 MB (95.61%)

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        stack = [0]
+
+        while stack:
+            room = stack.pop()
+            visited.add(room)
+
+            for key in rooms[room]:
+                if key not in visited:
+                    stack.append(key)
+
+        return len(visited) == len(rooms)
