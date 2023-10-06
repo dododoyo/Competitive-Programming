63f592698d4dbea02959f72e2f4b5e7b5508b9ec
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-10-06 18:37:30 +0300

    Time: 45 ms (28.21%), Space: 16.1 MB (97.76%)

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,23 +1,11 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        red,white,blue=0,0,0
+        freq = [0]*3
+        for i in nums:freq[i] += 1;
         
-        for i in nums:
-            if i == 0:
-                red += 1
-            elif i == 1:
-                white += 1
-            else:
-                blue += 1
-        j = 0
-        for i in range(red):
-            nums[j] = 0;j+=1
-        for i in range(white):
-            nums[j] = 1;j+=1
-        for i in range(blue):
-            nums[j] = 2;j+=1
+        for i in range(freq[0]):nums[i] = 0
+        for i in range(freq[1]):nums[i+freq[0]] = 1
+        for i in range(freq[2]):nums[i+freq[0]+freq[1]] = 2
+            
             
         return nums
