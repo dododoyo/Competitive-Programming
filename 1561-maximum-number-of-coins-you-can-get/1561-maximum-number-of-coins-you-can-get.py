7939ace50f1037073dc019855e51cbdedaccded4
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-10-11 17:54:28 +0300

    Time: 529 ms (46.78%), Space: 28.7 MB (33.05%)

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort()
-        bob,me,alice = 0,len(piles)-2,len(piles)-1
+        bob,me,alc = 0,len(piles)-2,len(piles)-1
         sol = 0
         while bob < me:
             sol += piles[me]
-            me -= 2
-            alice -= 2
-            bob += 1
-        return sol;
+            bob,me,alc = bob+1,me-2,alc-2
+        return sol
         
