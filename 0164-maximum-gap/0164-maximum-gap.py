568aec182e5f34306d9327df5d5978adcb749337
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-11-08 23:14:17 +0300

    Time: 864 ms (52.40%), Space: 30.9 MB (41.40%)

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,8 +1,9 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:  # Time: O(nlogn)
-        if len(nums) < 2:return 0
-        nums.sort()                           # O(nlogn)
-        sol = nums[1]-nums[0]                 # O(1)
-        for r in range(2,len(nums)):          #
-            sol = max(sol,nums[r]-nums[r-1])  # O(n)
-        return sol                            # O(1)
+        if len(nums) < 2:
+            return 0
+        nums.sort()
+        max_difference = 0
+        for right in range(1,len(nums)):
+            max_difference = max(max_difference,nums[right]-nums[right-1])
+        return max_difference
