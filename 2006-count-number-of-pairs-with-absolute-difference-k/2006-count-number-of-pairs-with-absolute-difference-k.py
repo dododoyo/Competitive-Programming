56cb4fdee04ce50193a7853e74b543ea6a96d200
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-11-15 22:41:58 +0300

    Time: 64 ms (84.49%), Space: 16.2 MB (75.96%)

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
+        num_freq = [0]*101
         solution = 0
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if abs(nums[i]-nums[j]) == k:
-                    solution += 1
-        return solution//2
-        
-        
+        for number in nums:
+            num_freq[number] +=1
+         
+        for right in range(k+1,101):
+            if num_freq[right] and num_freq[right-k]:
+                solution += num_freq[right]*num_freq[right-k]
+        return solution
+                
