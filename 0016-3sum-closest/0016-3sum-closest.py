276ef1cc2e20e66421fa8aa6e6251b688a3ad3db
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-11-17 10:23:51 +0300

    Time: 678 ms (43.78%), Space: 16.4 MB (17.14%)

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,24 +1,23 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        closest = sum(nums[:3])
-        nums.sort() # nlogn
-        for i in range(len(nums)-2):
-            left = i+1
+        # initialize variables
+        solution = sum(nums[:3])
+        # sort the array
+        nums.sort()
+        # use three pointers one for each element other for two 
+        for number in range(len(nums)-2):
+            left = number+1
             right = len(nums)-1
             current_sum = 0
             while left < right:
-                current_sum = nums[left]+nums[right]+nums[i]
-                if current_sum > target:
+                current_sum =  nums[left]+nums[right]+nums[number]
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
                     right -= 1
-                elif current_sum < target:
-                    left +=1
                 else:
-                    return target
-                if self.absLT(closest-target) > self.absLT(current_sum-target):
-                    closest = current_sum
-        return closest
-    def absLT(self,num):
-        return (-1*num) if num < 0 else num;
-                
-        
-        
+                    return current_sum
+                if (abs(current_sum-target) < abs(solution-target)):
+                    solution = current_sum
+        return solution
+    
