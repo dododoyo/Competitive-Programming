7831444482f4a1c8e7f36250f1892f16a11ddbc4
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-11-17 22:13:27 +0300

    Time: 736 ms (25.76%), Space: 22 MB (98.63%)

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -2,10 +2,9 @@ from collections import defaultdict
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         for index in range(len(nums)):
-            if nums[index] % 2 == 0:nums[index] = 0;
-            else:nums[index] = 1;
+            nums[index] = nums[index]%2;
         prefix_map = defaultdict(int)
-        current_sum,nice_count = 0,0
+        current_sum,nice_count = 0,0;
         for r in nums:
             current_sum += r
             if current_sum == k:nice_count += 1;
