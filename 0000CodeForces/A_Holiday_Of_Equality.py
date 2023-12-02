863c55d65ceb8dc8440770873309f49f6fe14579
dododoyo <dolphinmulugeta88@gmail.com>
2023-12-02 13:48:02 +0300

    Solved Holiday of Equality

--- 0000CodeForces/A_Holiday_Of_Equality.py
+++ 0000CodeForces/A_Holiday_Of_Equality.py
@@ -1,1 +1,7 @@
-for 
+input()
+citizens_welfare = list(map(int,input().split()))
+maximum_welfare = max(citizens_welfare);
+minimum_charge = 0;
+for each_welfare in citizens_welfare:
+  minimum_charge += maximum_welfare-each_welfare;
+print(minimum_charge)
