5c428d46aa70c3a10d804a95e16fc172b0214a88
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2023-12-14 21:47:13 +0300

    Added solution for Frequency Tracker

--- 0000A2SV/week_03/frequency-tracker.py
+++ 0000A2SV/week_03/frequency-tracker.py
@@ -2,46 +2,34 @@ class FrequencyTracker:
     def __init__(self):
         self.freq_dict = {}
         self.freq_set = {}
+
     def add(self, number: int) -> None:
         if number not in self.freq_dict:
             self.freq_dict[number] = 1
-            if 1 in self.freq_set:
-                self.freq_set[1].add(number)
-            else:
-                self.freq_set[1] = set([number])
+            if 1 in self.freq_set:self.freq_set[1].add(number)
+            else:self.freq_set[1] = set([number])
         else:
-            old_freq = self.freq_dict[number]
-            self.freq_set[old_freq].remove(number)
-
-            if not self.freq_set[old_freq]:
-                self.freq_set.pop(old_freq)
-
+            self.freq_set[self.freq_dict[number]].remove(number)
+            if not self.freq_set[self.freq_dict[number]]:
+                self.freq_set.pop(self.freq_dict[number])
             self.freq_dict[number] += 1
-
             if self.freq_dict[number] in self.freq_set:
                 self.freq_set[self.freq_dict[number]].add(number)
             else:
-                self.freq_set[self.freq_dict[number]] = set([number])
-                
-        
+                self.freq_set[self.freq_dict[number]] = set([number])    
     def deleteOne(self, number: int) -> None:
         if number in self.freq_dict:
             self.freq_set[self.freq_dict[number]].remove(number)
-
             if not self.freq_set[self.freq_dict[number]]:
                 self.freq_set.pop(self.freq_dict[number]) 
-                
             self.freq_dict[number] -= 1
-            
             if self.freq_dict[number] != 0:
                 if self.freq_dict[number] in self.freq_set: 
                     self.freq_set[self.freq_dict[number]].add(number)
                 else:
                     self.freq_set[self.freq_dict[number]] = set([number])
             else:
-                self.freq_dict.pop(number)
-                
-                
+                self.freq_dict.pop(number)    
 
     def hasFrequency(self, frequency: int) -> bool:
         if frequency in self.freq_set:
