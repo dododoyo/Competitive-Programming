2d6999eefd5da82b9a097b326a099a210d9d3000
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-01-01 23:54:56 +0300

    Added solution for Pancake Sorting

---
+++ 0000A2SV/week_06/pancake-sorting.py
@@ -0,0 +1,31 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        # let's first check if it is sorted 
+        sorted = True
+        for i in range(1,len(arr)):
+            if arr[i-1] > arr[i]:
+                sorted = False;break
+        if sorted:
+            return []
+        sol = []
+        for i in range(len(arr),0,-1):
+            flipIndex = self.getFlipIndex(arr,i)
+            if flipIndex != i:
+                if flipIndex != 1:
+                    sol.append(flipIndex)
+                    arr = self.flipArr(arr,len(arr),flipIndex)
+                sol.append(i)
+                arr = self.flipArr(arr,len(arr),i)
+        return sol
+
+    def getFlipIndex(self,arr,n):
+        for i in range(n):
+            if arr[i] == n:
+                return i+1
+            
+    def flipArr(self,arr,n,x):
+        sol = arr.copy()
+        for i in range(x):
+            sol[i] = arr[x-i-1]
+        return sol
+        
