bd3cb553503b7bc4bc03e0b32beb0f3c397e467c
dododoyo <dolphinmulugeta88@gmail.com>
2024-01-04 21:24:37 +0300

    Solved SegmentWSmallSum

---
+++ 0000CodeForces/A_Segment_with_Small_Sum.py
@@ -0,0 +1,11 @@
+n,s = map(int,input().split())
+a = [int(i) for i in input().split()]
+summ,solution = 0,0
+left = 0
+for right in range(len(a)):
+  summ += a[right]
+  while summ > s:
+    summ -= a[left]
+    left += 1
+  solution = max(solution,right-left+1)
+print(solution)
