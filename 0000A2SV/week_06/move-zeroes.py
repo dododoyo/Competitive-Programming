5c85e47036f4aa010f5e18dc2e7b02605f8b5bd5
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-01-05 19:31:12 -0300

    Added solution for Move Zeroes

--- 0000A2SV/week_06/move-zeroes.py
+++ 0000A2SV/week_06/move-zeroes.py
@@ -1,11 +1,8 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        left,right = 0,len(nums)-1
-        while left < right:
-            if nums[left] == 0:
-                for i in range(left,right):
-                    nums[i] = nums[i+1]
-                nums[right] = 0
-                right -= 1
-            else:
-                left += 1
+    def moveZeroes(self, nums: list) -> None:
+        holder = 0
+        for seeker in range(len(nums)):
+            if nums[seeker] != 0 and nums[holder] == 0:
+                nums[holder], nums[seeker] = nums[seeker], nums[holder]
+            if nums[holder] != 0:
+                holder += 1
