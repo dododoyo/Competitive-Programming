327d6cd248c23c505bfd9051e87ded24a68653a1
dododoyo <dolphinmulugeta88@gmail.com>
2024-01-08 21:27:41 +0300

    Fixed SegmentSmallSum(CF)

--- 0000CodeForces/A_Segment_with_Small_Sum.py
+++ 0000CodeForces/A_Segment_with_Small_Sum.py
@@ -4,7 +4,7 @@ summ,solution = 0,0
 left = 0
 for right in range(len(a)):
   summ += a[right]
-  while summ > s:
+  while summ >= s:
     summ -= a[left]
     left += 1
   solution = max(solution,right-left+1)
