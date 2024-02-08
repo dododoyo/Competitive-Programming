d3010f2db85269b7692e43e3d7b7006c09c4ee4f
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-08 15:50:19 +0300

    Add solution for Maximum Score After Splitting a String

--- 0000A2SV/camp_01/week_01/leetcode/maximum-score-after-splitting-a-string.py
+++ 0000A2SV/camp_01/week_01/leetcode/maximum-score-after-splitting-a-string.py
@@ -5,12 +5,18 @@ class Solution:
         for i in range(1,len(s)):
             zero_count[i] = (int(s[i]=="0")) + zero_count[i-1]
             one_count[len(s)-i-1] = int(s[len(s)-i-1]) + one_count[len(s)-i]
+
+        # the first one is never counted in ones
         one_count[0] = one_count[1]
+        # the last zero is never counted in zeros 
         zero_count[-1] = zero_count[-2]
-        if len(s) == one_count[0]:
-            return one_count[1]
-        if len(s) == zero_count[-1]:
-            return zero_count[-2]
+
+        #handeling edge case when all the elements are one
+        if len(s) == one_count[0]:return one_count[1]
+
+        #handeliing the edge case when all the elements are zeros
+        if len(s) == zero_count[-1]:return zero_count[-2]
+        
         solution = one_count[0] + zero_count[0]
         for i in range(len(s)):
             solution = max(solution,one_count[i]+zero_count[i])
