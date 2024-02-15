675c240f368655f8aca12c47399828ab8f7fe9ed
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-15 16:32:25 +0300

    Add solution for Maximum Element After Decreasing and Rearranging

--- 0000A2SV/camp_01/week_02/leetcode/maximum-element-after-decreasing-and-rearranging.py
+++ 0000A2SV/camp_01/week_02/leetcode/maximum-element-after-decreasing-and-rearranging.py
@@ -1,9 +1,20 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
-        arr.sort()
-        arr[0] = 1
-        for i in range(1,len(arr)):
-            if arr[i] > arr[i-1] +1:
-                arr[i] = arr[i-1] +1 
-        return arr[-1]
-        
+        extras,num_map = 0,defaultdict(int)
+        current_number = maximum_solution = len(arr)
+
+        # count the elements occurance
+        for number in arr:
+            if number > maximum_solution:extras += 1
+            else:num_map[number] += 1
+
+        # get the maximum number you can get
+        while current_number:
+            if  current_number in num_map:
+                extras += num_map[current_number] - 1
+            else:
+                if extras > 0:extras -= 1
+                else:maximum_solution -= 1 
+            current_number -= 1
+            
+        return maximum_solution
