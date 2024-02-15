93fcc264244b8d783f8efa620b53a300ecc4af70
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-15 15:20:37 +0300

    Add solution for Maximum Element After Decreasing and Rearranging

---
+++ 0000A2SV/camp_01/week_02/leetcode/maximum-element-after-decreasing-and-rearranging.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1,len(arr)):
+            if arr[i] > arr[i-1] +1:
+                arr[i] = arr[i-1] +1 
+        return arr[-1]
+        
