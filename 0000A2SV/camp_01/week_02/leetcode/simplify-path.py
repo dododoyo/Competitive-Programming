0fcc927b70c67597fbd9f6a74112ddcb3f9661e2
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-16 09:58:19 +0300

    Add solution for Simplify Path

---
+++ 0000A2SV/camp_01/week_02/leetcode/simplify-path.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        solution = []
+        commands = path.split("/")
+        print(commands)
+        for command in commands:
+            if command == "..":
+                if solution:
+                    solution.pop()
+            elif command not in ("","."):
+                # command is  a word
+                solution.append(command)
+        return "/" + "/".join(solution)
+        
