e8eaaa9c386ca1a2d80d7929765680877b022a16
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-20 18:51:11 +0300

    Add solution for Merge Two Sorted Lists

--- 0000A2SV/camp_01/week_02/leetcode/merge-two-sorted-lists.py
+++ 0000A2SV/camp_01/week_02/leetcode/merge-two-sorted-lists.py
@@ -5,35 +5,17 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 == None:return list2
-        elif list2 == None:return list1
-        if list1.val < list2.val:
-            merged_list = list1
-            list1 = list1.next
-        else:
-            merged_list = list2
-            list2 = list2.next
-        tail = merged_list
-        while list1 and list2:
-            if list1.val < list2.val:
-                next_list1 = list1.next
-                tail.next = list1
-                tail = tail.next
-                list1 = next_list1
-            else:
-                next_list2 = list2.next
-                tail.next = list2
-                tail = tail.next
-                list2 = next_list2
-        while list1:
-                next_list1 = list1.next
-                tail.next = list1
-                tail = tail.next
-                list1 = next_list1
-        while list2:
-            next_list2 = list2.next
-            tail.next = list2
-            tail = tail.next
-            list2 = next_list2
+        return self.helper(list1,list2)
 
-        return merged_list
+    def helper(self, list1, list2):
+        if list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
+        else:
+            if list1.val < list2.val:
+                list1.next = self.helper(list1.next, list2)
+                return list1
+            else:
+                list2.next = self.helper(list1, list2.next)
+                return list2
