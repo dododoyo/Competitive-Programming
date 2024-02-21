eff0eb51984b96cf65c641607bcfd9f010024333
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-21 13:15:58 +0300

    Add solution for Separate Black and White Balls

---
+++ 0000A2SV/camp_01/week_02/leetcode/separate-black-and-white-balls.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        solution,ones_count = 0,0
+        """
+        When we see a one we count
+        When we see a zero we add the number of ones counted before it 
+        Because all of the ones before it has to be swapped"""
+        for i in range(len(s)):
+            if s[i] == "1":
+                ones_count += 1
+            else:
+                solution += ones_count
+        return solution
+        
