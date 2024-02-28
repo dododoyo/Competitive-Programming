2c084af65260a4ffd9eb33e9398e8089382521ef
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-02-28 09:30:35 +0300

    Add solution for Time Needed to Buy Tickets

---
+++ 0000A2SV/camp_01/week_04/leetcode/time-needed-to-buy-tickets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        current, solution = 0, 0
+        # simulate the process until you get the answer
+        while True:
+            if tickets[current]:
+                tickets[current] -= 1
+                solution += 1
+
+            if current == k and tickets[current] == 0:
+                return solution
+
+            current += 1
+            
+            # start the loop again
+            if current == len(tickets):
+                current = 0
