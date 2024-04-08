9a629aaf6153471340a4ed32de74a041c1dc411b
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-04-08 08:46:56 -0300

    Created 'A - Flag Checker' by A2SV Hub

---
+++ A2SV G5 - Contest #11 08-Apr-2024/A - Flag Checker 118350.py
@@ -0,0 +1,13 @@
+# Problem: A - Flag Checker - https://codeforces.com/gym/515998/problem/A
+
+n, m = map(int, input().split())
+ans = 'YES'
+prev = {""}
+
+for _ in range(n):
+    sr = set(input())
+    if len(sr) > 1 or prev == sr:
+        ans = 'NO'
+    prev = sr
+
+print(ans)
