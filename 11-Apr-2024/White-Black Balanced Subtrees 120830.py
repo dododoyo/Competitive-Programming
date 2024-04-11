4195e7f5bfc06ed6e36b2f43d51afa42e1852b60
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-04-11 18:08:01 +0300

    Created 'White-Black Balanced Subtrees' by A2SV Hub

---
+++ 11-Apr-2024/White-Black Balanced Subtrees 120830.py
@@ -0,0 +1,45 @@
+# Problem: White-Black Balanced Subtrees - https://codeforces.com/contest/1676/problem/G
+
+from collections import defaultdict
+import sys
+import threading
+
+def input(): return sys.stdin.readline().strip()
+
+
+def main():
+    for _ in range(int(input())):
+      n = int(input())
+      parents = [int(num) for num in input().split()]
+      node_color = input()
+
+      graph, solution = defaultdict(list), 0
+      for index, node in enumerate(parents):
+        graph[node].append(index+2)
+
+      def dfs(node):
+        nonlocal solution
+        children = len(graph[node])
+        if not children:
+          return 1 if node_color[node-1] == "W" else -1
+
+        children_sum = 0
+        for child in graph[node]:
+          child_val = dfs(child)
+          solution += (child_val == 0)
+          children_sum += child_val
+
+        return ((1 if node_color[node-1] == "W" else -1) + children_sum)
+
+      whole_tree = dfs(1)
+      solution += (whole_tree == 0)
+      print(solution)
+
+if __name__ == '__main__':
+
+    sys.setrecursionlimit(1 << 30)
+    threading.stack_size(1 << 27)
+
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    main_thread.join()
