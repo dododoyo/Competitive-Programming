2bcd9930745a2f6f73f4a8d0b81a81cf9a4a405e
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-04-25 00:04:57 +0300

    Created 'Fox And Names  (codeforces)' by A2SV Hub

---
+++ 24-Apr-2024/Fox And Names  (codeforces) 132407.py
@@ -0,0 +1,42 @@
+# Problem: Fox And Names  (codeforces) - https://codeforces.com/contest/510/problem/C
+
+def dfs(node):
+  visited[node] = 1
+  for neighbor in graph[node]:
+    if visited[neighbor] == 1:
+      print("Impossible")
+      exit()
+    if visited[neighbor] == 0:
+      dfs(neighbor)
+  visited[node] = 2
+  output_order.append(chr(node+ord("a")))
+
+
+
+n = int(input())
+first_name = input()
+graph,visited = [set() for _ in range(26)],[0]*26
+
+for _ in range(1, n):
+  current_name,i = input(),0
+
+  while i < min(len(current_name), len(first_name)) and (current_name[i] == first_name[i]):
+    i += 1
+
+  if (i < min(len(first_name), len(current_name))) and (current_name[i] != first_name[i]):
+    graph[ord(first_name[i])-ord("a")].add(ord(current_name[i])-ord("a"))
+
+  elif len(first_name) > len(current_name):
+    print("Impossible")
+    exit()
+  first_name = current_name
+
+output_order = []
+
+for i in range(26):
+  if visited[i] == 0:
+    dfs(i)
+    
+output_order.reverse()
+
+print("".join(output_order))
