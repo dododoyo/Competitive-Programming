06372f6709f4a68bf4c7c9ab6d094480e1777c7b
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-05-01 19:23:21 +0300

    Created ' Add Binary' by A2SV Hub

---
+++ 01-May-2024/ Add Binary 138484.py
@@ -0,0 +1,6 @@
+# Problem:  Add Binary - https://leetcode.com/problems/add-binary/
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a,2)+int(b,2))[2:]
+        
