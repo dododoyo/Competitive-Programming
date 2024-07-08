245ff9b48d23dffd03ce03849ea085996a0f2e1d
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-07-08 23:42:01 +0300

    Created 'Maximum Gap' by A2SV Hub

---
+++ 08-Jul-2024/Maximum Gap 177965.py
@@ -0,0 +1,11 @@
+# Problem: Maximum Gap - https://leetcode.com/problems/maximum-gap/
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:  # Time: O(nlogn)
+        if len(nums) < 2:
+            return 0
+        nums.sort()
+        max_difference = 0
+        for right in range(1,len(nums)):
+            max_difference = max(max_difference,nums[right]-nums[right-1])
+        return max_difference
