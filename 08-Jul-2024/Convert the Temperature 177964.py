62509a7c8c0775e14cd8ad18669892afdcc8f09c
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-07-08 23:40:56 +0300

    Created 'Convert the Temperature' by A2SV Hub

---
+++ 08-Jul-2024/Convert the Temperature 177964.py
@@ -0,0 +1,5 @@
+# Problem: Convert the Temperature - https://leetcode.com/problems/convert-the-temperature/
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15,celsius*1.8 + 32]
