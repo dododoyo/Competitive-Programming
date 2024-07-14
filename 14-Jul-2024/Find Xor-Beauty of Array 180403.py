2735dc889c6ac699dc38ee3d2d54222986c2655c
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-07-14 23:40:41 +0300

    Created 'Find Xor-Beauty of Array' by A2SV Hub

---
+++ 14-Jul-2024/Find Xor-Beauty of Array 180403.py
@@ -0,0 +1,9 @@
+# Problem: Find Xor-Beauty of Array - https://leetcode.com/problems/find-xor-beauty-of-array/
+
+class Solution:
+    def xorBeauty(self, nums: List[int]) -> int:
+        solution=0
+        for num in nums:
+            solution=solution^num
+        return solution
+        
