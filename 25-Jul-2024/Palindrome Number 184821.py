ac1c2f29523e9c6c955c67686ff7037a61ea37cf
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-07-25 23:38:00 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 25-Jul-2024/Palindrome Number 184821.py
@@ -0,0 +1,8 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        x = str(x)
+        return x == x[::-1]
