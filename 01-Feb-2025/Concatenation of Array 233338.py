9fbf02d82552d42fa1f37ae5a9721491c2212539
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2025-02-01 14:01:05 +0300

    Created 'Concatenation of Array' by A2SV Hub

---
+++ 01-Feb-2025/Concatenation of Array 233338.py
@@ -0,0 +1,5 @@
+# Problem: Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
